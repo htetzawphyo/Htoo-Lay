e9f2139a025b0c594b52333de01ca24bf163b4fc
htetzawphyo <htetzawphyo.hpn@gmail.com>
2024-02-09 00:51:27 +0630

    add new logo

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Htoo Lay' }}</title>
+        <title>{{ $title ?? 'Charming Treasures' }}</title>
         
         {{-- @vite('resources/css/app.css') --}}
         @vite(['resources/css/app.css', 'resources/js/app.js'])
--- resources/views/livewire/front/home.blade.php
+++ resources/views/livewire/front/home.blade.php
@@ -29,48 +29,17 @@ $getFilteredProducts = function ()
 
     return $query->orderBy('created_at', 'desc')->paginate(10);
 }
-
-// with(fn () => [
-//     'products' => Product::with('category', 'discount')
-//                     ->where('name', 'like', '%' . $this->search . '%')
-//                     ->orWhere('product_code', 'like', '%' . $this->search . '%')->paginate(10),
-// ]);
-
-// with(fn () => [
-//     $products = $this->filterId ? Product::with('category', 'discount')->whereCategoryId($this->filterId) : Product::with('category', 'discount')->where('name', 'like', '%' . $this->search . '%')->orWhere('product_code', 'like', '%' . $this->search . '%'),
-    
-//     'products' => $products->orderBy('created_at', 'desc')->paginate(10),
-// ]);
 ?>
 
 <div class="bg-white text-gray-600 work-sans leading-normal text-base tracking-normal">
     <!--Nav-->
-    <nav id="header" class="w-full z-30 top-0 py-1">
+    <nav id="header" class="w-full z-30 top-0 py-1 bg-gray-400">
         <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-6 py-3">
             <a href="/" class="flex items-center tracking-wide no-underline hover:no-underline font-primary font-bold text-gray-800 text-2xl ">
-                <i class="fa-solid fa-hand-sparkles fill-current text-violet-800 mr-2"></i>
+                <!-- <i class="fa-solid fa-hand-sparkles fill-current text-violet-800 mr-2"></i> -->
+                <img src="/logo.png" alt="logo" class="w-8 rounded-sm mr-2">
                 <span class="bg-gradient-to-r from-stone-900 via-violet-500 to-violet-900 text-transparent bg-clip-text">Charming Treasures 20</span>
             </a>
-            
-
-            {{-- <div class="order-2 md:order-3 flex items-center" id="nav-content">
-
-                <a class="inline-block no-underline hover:text-violet-950" href="#">
-                    <svg class="fill-current text-violet-800 hover:text-violet-950" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                        <circle fill="none" cx="12" cy="7" r="3" />
-                        <path d="M12 2C9.243 2 7 4.243 7 7s2.243 5 5 5 5-2.243 5-5S14.757 2 12 2zM12 10c-1.654 0-3-1.346-3-3s1.346-3 3-3 3 1.346 3 3S13.654 10 12 10zM21 21v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h2v-1c0-2.757 2.243-5 5-5h4c2.757 0 5 2.243 5 5v1H21z" />
-                    </svg>
-                </a>
-
-                <a class="pl-3 inline-block no-underline hover:text-violet-950" href="#">
-                    <svg class="fill-current text-violet-800 hover:text-violet-950" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                        <path d="M21,7H7.462L5.91,3.586C5.748,3.229,5.392,3,5,3H2v2h2.356L9.09,15.414C9.252,15.771,9.608,16,10,16h8 c0.4,0,0.762-0.238,0.919-0.606l3-7c0.133-0.309,0.101-0.663-0.084-0.944C21.649,7.169,21.336,7,21,7z M17.341,14h-6.697L8.371,9 h11.112L17.341,14z" />
-                        <circle cx="10.5" cy="18.5" r="1.5" />
-                        <circle cx="17.5" cy="18.5" r="1.5" />
-                    </svg>
-                </a>
-
-            </div> --}}
         </div>
     </nav>
 
@@ -91,95 +60,50 @@ $getFilteredProducts = function ()
                 </div>
             </div>
         @endif
-        
-        {{-- <div>
-            <div class="mb-3 flex justify-start ms-20">
-                <select id="pricingType" name="pricingType"
-                class="w-6/12 h-9 mx-2 border-2 border-gray-500 focus:outline-none focus:border-gray-500 text-gray-500 rounded px-2 md:px-3 py-0 md:py-1 tracking-wider">
-                    <option value="All" selected="">All</option>
-                    <option value="Freemium">Freemium</option>
-                    <option value="Free">Free</option>
-                    <option value="Paid">Paid</option>
-                </select>
-            </div>
-            
-            <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">           
-                @foreach ($products as $product)                
-                    <div class="w-full rounded-lg shadow-lg md:w-1/3 xl:w-1/4 p-6 flex flex-col">
-                        <div>
-                            <img class="hover:grow hover:shadow-lg " src="{{asset('storage/product-image/'.$product->image)}}">
-                            <div class="pt-3 mb-3 flex items-center justify-between">
-                                <p class="text-violet-800 font-primary font-bold">
-                                    {{ $product->name }} 
-                                    <span class="text-xs text-gray-800"> - {{ $product->product_code }}</span>
-                                </p>
-                                <div class="">
-                                    @if ($product->discount)
-                                        <s class="pt-1 text-gray-900 inline-block">{{ $product->price}} Ks</s>
-                                        <p class="pt-1 text-red-600 inline-block">{{ $product->price - $product->discount->discount_amount }} Ks</p>
-                                    @else                                    
-                                        <p class="pt-1 text-gray-900">{{ $product->price }} Ks</p>
-                                    @endif
-                                </div>
+
+        <div class="mb-3 ms-3 flex justify-start">
+            <select id="filter" wire:model.live="filterId"  name="filter" class="h-9 mx-2 border-2 border-gray-500 focus:outline-none focus:border-gray-500 text-gray-500 rounded px-2 md:px-3 py-0 md:py-1 tracking-wider">
+                <option value="0" selected>All</option>
+                @foreach (Category::all() as $category)                        
+                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="grid grid-cols-4 max-md:grid-cols-3 max-sm:grid-cols-2 mx-5 gap-5">
+            @foreach ($products as $product)
+                <div class="rounded-lg shadow-lg p-6 flex flex-col bg-neutral-200">
+                    <div>
+                        <img class="hover:grow hover:shadow-lg " src="{{asset('storage/product-image/'.$product->image)}}">
+                        <div class="pt-3 mb-3 flex flex-col">
+                            <p class="text-violet-800 font-primary font-bold">
+                                {{ $product->name }} 
+                                <span class="text-xs text-gray-800"> - {{ $product->product_code }}</span>
+                            </p>
+                            <div class="">
+                                @if ($product->discount)
+                                    <s class="pt-1 text-gray-900 inline-block">{{ $product->price}} Ks</s>
+                                    <p class="pt-1 text-red-600 inline-block">{{ $product->price - $product->discount->discount_amount }} Ks</p>
+                                @else                                    
+                                    <p class="pt-1 text-gray-900">{{ $product->price }} Ks</p>
+                                @endif
                             </div>
                         </div>
-                        <div class="flex justify-between">
-                            <a href="" class="font-primary inline-block rounded bg-violet-500 text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] hover:bg-violet-600 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-violet-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] active:bg-violet-700 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal transition duration-150 ease-in-out focus:outline-none focus:ring-0">View Detail</a>
-                            @if ($product->instock)                            
-                                <span class="bg-green-800 text-red-100 text-xs font-medium me-2 px-2 flex items-center rounded-full ">Instock</span>
-                            @else                            
-                                <span class="bg-red-800 text-red-100 text-xs font-medium me-2 px-2 flex items-center rounded-full ">Out of Stock</span>
-                            @endif
-                        </div>
                     </div>
-                @endforeach
-            </div>
-        </div> --}}
-
-
-        <div class="flex items-center flex-wrap pt-4 pb-12 mx-6">
-            <div class="mb-3 flex justify-start">
-                <select id="filter" wire:model.live="filterId"  name="filter" class="h-9 mx-2 border-2 border-gray-500 focus:outline-none focus:border-gray-500 text-gray-500 rounded px-2 md:px-3 py-0 md:py-1 tracking-wider">
-                    <option value="0" selected>All</option>
-                    @foreach (Category::all() as $category)                        
-                        <option value="{{ $category->id }}">{{ $category->name }}</option>
-                    @endforeach
-                </select>
-            </div>
-            
-            <div class="flex flex-wrap">
-                @foreach ($products as $product)
-                    <div class="w-full rounded-lg shadow-lg md:w-1/3 xl:w-1/4 p-6 flex flex-col">
-                        <div>
-                            <img class="hover:grow hover:shadow-lg " src="{{asset('storage/product-image/'.$product->image)}}">
-                            <div class="pt-3 mb-3 flex items-center justify-between">
-                                <p class="text-violet-800 font-primary font-bold">
-                                    {{ $product->name }} 
-                                    <span class="text-xs text-gray-800"> - {{ $product->product_code }}</span>
-                                </p>
-                                <div class="">
-                                    @if ($product->discount)
-                                        <s class="pt-1 text-gray-900 inline-block">{{ $product->price}} Ks</s>
-                                        <p class="pt-1 text-red-600 inline-block">{{ $product->price - $product->discount->discount_amount }} Ks</p>
-                                    @else                                    
-                                        <p class="pt-1 text-gray-900">{{ $product->price }} Ks</p>
-                                    @endif
-                                </div>
-                            </div>
-                        </div>
-                        <div class="flex justify-between">
-                            {{-- <a href="" class="font-primary inline-block rounded bg-violet-500 text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] hover:bg-violet-600 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-violet-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] active:bg-violet-700 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal transition duration-150 ease-in-out focus:outline-none focus:ring-0">View Detail</a> --}}
-                            @if ($product->instock)                            
-                                <span class="bg-green-800 text-red-100 text-xs font-medium me-2 px-2 flex items-center rounded-full ">Instock</span>
-                            @else                            
-                                <span class="bg-red-800 text-red-100 text-xs font-medium me-2 px-2 flex items-center rounded-full ">Out of Stock</span>
-                            @endif
-                        </div>
+                    <div class="flex justify-between">
+                        @if ($product->instock)                            
+                            <span class="bg-green-800 text-red-100 text-xs font-medium me-2 px-2 flex items-center rounded-full ">Instock</span>
+                        @else                            
+                            <span class="bg-red-800 text-red-100 text-xs font-medium me-2 px-2 flex items-center rounded-full ">Out of Stock</span>
+                        @endif
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
         </div>
         
-        {{ $products->withQueryString()->links(data: ['scrollTo' => false]) }}
+        
+        <div class="mt-8 mx-5 mb-5">
+            {{ $products->withQueryString()->links(data: ['scrollTo' => false]) }}
+        </div>
     </section>
 </div>
